935ecc412e78309999ce1fa613b19b45a1451ea6
Nejc Jenko <nejc.jenko1@gmail.com>
2020-08-28 14:53:51 +0200

    vmesni commit

---
+++ model.py
@@ -0,0 +1,21 @@
+class Ekipa:
+    def __init__(self, ime, seed1, ekskluzivni):
+        self.ime = ime
+        self.seed1 = seed1
+        self.ekskluzivni = ekskluzivni
+        self.igre = []
+
+    def dodaj_igro()
+
+
+class Igra:
+    def __init__(self, winner, loser):
+        self.winner = winner
+        self.loser = loser
+
+
+class Skupina:
+    def __init__(self):
+        self.ekipe = []
+
+        
