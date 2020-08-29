c0f8af836bc446e336e52ceb291f4419bb192224
Nejc Jenko <nejc.jenko1@gmail.com>
2020-08-29 14:22:02 +0200

    dodajanje razreda Turnir

--- model.py
+++ model.py
@@ -1,21 +1,30 @@
+class Turnir:
+    def __init__(self, stevilo_skupin):
+        self.stevilo_skupin = stevilo_skupin
+        self.skupine = []
+
+
+class Skupina:
+    def __init__(self, indeks_skupine):
+        self.ekipe = []
+ 
+
+
 class Ekipa:
     def __init__(self, ime, seed1, ekskluzivni):
         self.ime = ime
         self.seed1 = seed1
         self.ekskluzivni = ekskluzivni
-        self.igre = []
+        self.odigrane_igre = []
 
-    def dodaj_igro()
+    def dodaj_igro(self, winner, loser):
+        igra = Igra(winner, loser)
+        self.odigrane_igre.append(igra)
 
 
 class Igra:
     def __init__(self, winner, loser):
         self.winner = winner
         self.loser = loser
-
-
-class Skupina:
-    def __init__(self):
-        self.ekipe = []
 
         
